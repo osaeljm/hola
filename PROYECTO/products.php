6d209c5d61b0b4f357eec744fb57edca8a46874f
emCESAR/em <emcesarjretanaj@gmail.com/em>
2014-11-08 14:02:41 -0600

    Actualizacion product

--- PROYECTO/products.php
+++ PROYECTO/products.php
@@ -128,27 +128,13 @@ http://www.templatemo.com/preview/templatemo_417_grill
                         //current URL of the Page. cart_update.php redirects back to this URL
                         $current_url = base64_encode("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
                             
-                           $results = $mysqli->query("SELECT * FROM producto ORDER BY IdProducto ASC");
+                           $results = $mysqli->query("SELECT * FROM Producto ORDER BY IdProducto ASC");
                             if ($results) { 
                                 //output results from database
                                 while($obj = $results->fetch_object())
                                 { 
                         ?>                      
-                                    <!-- echo '<div class="product">'; 
-                                    echo '<form method="post" action="cart_update.php">';
-                                    echo '<div class="product-thumb"><img src="images/'.$obj->product_img_name.'"></div>';
-                                    echo '<div class="product-thumb">'.$obj->product_qty.'</div>';
-                                    echo '<div class="product-content"><h3>'.$obj->product_name.'</h3>';
-                                    echo '<div class="product-desc">'.$obj->product_desc.'</div>';
-                                    echo '<div class="product-info">Price '.$currency.$obj->price.' <button class="add_to_cart">Add To Cart</button></div>';
-                                    echo '</div>';
-                                    echo '<input type="hidden" name="product_code" value="'.$obj->product_code.'" />';
-                                    echo '<input type="hidden" name="type" value="add" />';
-                                    echo '<input type="hidden" name="return_url" value="'.$current_url.'" />';
-                                    echo '</form>';
-                                    echo '</div>'; -->
-
-                               
+                                                              
                                     <form method="post" action="cart_update.php">
                                         <div class="col-md-3 col-sm-6 mix portfolio-item Pizza">       
                                             <div class="portfolio-wrapper">                
@@ -177,7 +163,6 @@ http://www.templatemo.com/preview/templatemo_417_grill
                                 }               
                             }
                         ?>
-
 
 
 
